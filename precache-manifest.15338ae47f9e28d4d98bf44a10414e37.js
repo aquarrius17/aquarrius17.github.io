2ef1bda65364d4ea153ce82d928d95bd54380c1b
hakka-hidden <hakka-hidden@localhost.localdomain>
2020-07-03 22:20:52 +0700

    initial commit

---
+++ precache-manifest.15338ae47f9e28d4d98bf44a10414e37.js
@@ -0,0 +1,109 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dcea1afaae8c20eaecdfb68af60a722",
+    "url": "js/register-sw.js"
+  },
+  {
+    "url": "manifest.755610a81c6cd0a16f440020fd0618c9.json"
+  },
+  {
+    "revision": "1e22afe0a9bae8bd0a24",
+    "url": "matchDetail.js"
+  },
+  {
+    "revision": "270f6255d4e1e5c5f7b07e23b9692d04",
+    "url": "match.html"
+  },
+  {
+    "revision": "9d5e2936115ad564c967799c9faaa233",
+    "url": "matchDetail.html"
+  },
+  {
+    "revision": "0d357b73a4929873d5043a5e26871a77",
+    "url": "nav.html"
+  },
+  {
+    "revision": "01f73c042917b4e991f26473118ecd50",
+    "url": "pages/top_scorers.html"
+  },
+  {
+    "revision": "c1e7db7b8ec79c7107544c0f10dacdf1",
+    "url": "pages/standings.html"
+  },
+  {
+    "revision": "79a671105fcd158608bcfa83ccefb63a",
+    "url": "pages/saved_matches.html"
+  },
+  {
+    "revision": "c09085234ed1ef3c54d26d6a0ef57d3c",
+    "url": "pages/home.html"
+  },
+  {
+    "revision": "f0fd79bbe4a7585f552f69c4092b2dd7",
+    "url": "pages/about.html"
+  },
+  {
+    "revision": "bd154e7515ed2ee4456e",
+    "url": "match.js"
+  },
+  {
+    "revision": "0d6383b6a6da1acd8fa1f6de129ff715",
+    "url": "js/component/nav-detail.js"
+  },
+  {
+    "revision": "ea6239d794765db06aa045a925fb8eee",
+    "url": "js/register-push.js"
+  },
+  {
+    "revision": "a66942528a8af114e8a0ae4b517ab0be",
+    "url": "js/idb.js"
+  },
+  {
+    "revision": "2ef60067f27b38f93ca9e6bf48fc10ed",
+    "url": "js/component/foot-bar.js"
+  },
+  {
+    "revision": "d25a1847a79490bc2ec23ba5929e3606",
+    "url": "js/component/get-scorers.js"
+  },
+  {
+    "revision": "8831fbe859e2ada9c1f9419e8af03744",
+    "url": "js/component/nav-bar.js"
+  },
+  {
+    "revision": "1f256b65fc025daf527cd1f2442afffc",
+    "url": "js/component/nav-button.js"
+  },
+  {
+    "revision": "d1317bb2acdfb3c5b13176eeccc3ccc2",
+    "url": "js/nav.js"
+  },
+  {
+    "revision": "d5de536adaa9a004c7dc79a7c2e3fb3c",
+    "url": "js/component/get-matches.js"
+  },
+  {
+    "revision": "a74ff24a6dc370aed8bf963116233d23",
+    "url": "js/foot.js"
+  },
+  {
+    "revision": "8b3a5d65b57a181d43e40811e49df20b",
+    "url": "js/db.js"
+  },
+  {
+    "revision": "eb2ee9ca1c4de299a36767b3f0ab49a1",
+    "url": "js/api.js"
+  },
+  {
+    "revision": "0f8cbf2d6c15f18c4335",
+    "url": "index.js"
+  },
+  {
+    "revision": "171c3ca5106654d9d8ebb7ea645b8274",
+    "url": "index.html"
+  },
+  {
+    "revision": "ccddb83aac656dfe27b565a2dc448862",
+    "url": "css/style.css"
+  }
+];
